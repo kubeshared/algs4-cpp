0dbfa488fe63447043456be1e1cf2ddff220c12e
kubeshared <markhub.cn@gmail.com>
2021-01-21 23:08:43 +0800

    Add some API implementation for Edgeweight graph

--- graph/edge_weighted_graph.cpp
+++ graph/edge_weighted_graph.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "edge_weighted_graph.h"
+
+EdgeWeightedGraph::EdgeWeightedGraph(int v) {
+    this->v = v;
+    this->e = 0;
+    this->g = vector<vector<Edge>>(this->v, vector<Edge>());
+}
+
+EdgeWeightedGraph::EdgeWeightedGraph(fstream &in) {
+    int v, e;
+    in >> v >> e;
+    this->v = v;
+    this->e = 0;
+    this->g = vector<vector<Edge>>(this->v, vector<Edge>());
+    for (int i = 0; i < e; i++) {
+        int v, w;
+        double weight;
+        in >> v >> w >> weight;
+        addEdge(Edge(v, w, weight));
+    }
+}
+
+void EdgeWeightedGraph::addEdge(Edge e) {
+    int v = e.either();
+    int w = e.other(v);
+    g[v][w] = e;
+    g[w][v] = e;
+}
+
+AdjacencyIterator<Edge>* EdgeWeightedGraph::adj(int v) {
+
+}
--- graph/edge_weighted_graph.h
+++ graph/edge_weighted_graph.h
@@ -5,9 +5,27 @@
 #ifndef ALGS4_EDGE_GRAPH_H
 #define ALGS4_EDGE_GRAPH_H
 
+#include "graph.h"
+#include "edge.h"
+#include <fstream>
+
+using namespace std;
 
 class EdgeWeightedGraph {
+public:
+    EdgeWeightedGraph(int v);
+    EdgeWeightedGraph(fstream& in);
+    int V() const;
+    int E() const;
+    void addEdge(Edge e);
+    AdjacencyIterator<Edge>* adj(int v);
+    vector<Edge> edges();
+    string toString();
 
+private:
+    int v;
+    int e;
+    vector<vector<Edge>> g;
 };
 
 
